6cd8deaf6cc2b730507f9cc10fa60215737e62ec
drshnn <darshansu156@gmail.com>
2024-07-17 22:30:06 +0530

    added UploadPage component

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import UploadPage from './pages/UploadPage/UploadPage.tsx';
 
 
 const router = createBrowserRouter([
@@ -12,12 +13,14 @@ const router = createBrowserRouter([
   },
   {
     path: "/upload",
-    element: <div>Upload Page</div>,
+    element: <UploadPage></UploadPage>,
   }
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <div className="wrap font-inter">
+      <RouterProvider router={router} />
+    </div>
   </React.StrictMode>,
 )
