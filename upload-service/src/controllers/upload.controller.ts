16159ceb68ed0fbed95fd63f759f2f1fe2749011
Darshan Suthar <darshansu156@gmail.com>
2024-08-10 13:51:26 +0530

    removed local stack endpoint url

--- upload-service/src/controllers/upload.controller.ts
+++ upload-service/src/controllers/upload.controller.ts
@@ -1,123 +1,130 @@
 import { NextFunction, Request, Response } from "express";
 import {
-    CreateMultipartUploadCommand,
-    UploadPartCommand,
-    CompleteMultipartUploadCommand,
-    AbortMultipartUploadCommand,
-    S3Client,
-    ListPartsCommand,
+  CreateMultipartUploadCommand,
+  UploadPartCommand,
+  CompleteMultipartUploadCommand,
+  ListPartsCommand,
 } from "@aws-sdk/client-s3";
 import { getAwsClient } from "../utils/awsClient";
 import { InternalServerError } from "../errors/app.error";
 
-export const initUpload = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        console.log('Initialising Upload');
-        const { fileName } = req.body;
-        console.log(req.body);
-        const bucketName = process.env.AWS_BUCKET_NAME;
-        console.log(bucketName);
+export const initUpload = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    console.log("Initialising Upload");
+    const { fileName } = req.body;
+    console.log(req.body);
+    const bucketName = process.env.AWS_BUCKET_NAME;
+    console.log(bucketName);
 
-        const s3Client = getAwsClient();
-        const createParams = {
-            Bucket: bucketName,
-            Key: fileName,
-            ContentType: 'video/mp4'
-        };
+    const s3Client = getAwsClient();
+    const createParams = {
+      Bucket: bucketName,
+      Key: fileName,
+      ContentType: "video/mp4",
+    };
 
-        const multipartParams = await s3Client.send(
-            new CreateMultipartUploadCommand(createParams)
-        )
-        console.log("multipartparams---- ", multipartParams);
-        const uploadId = multipartParams.UploadId;
+    const multipartParams = await s3Client.send(
+      new CreateMultipartUploadCommand(createParams)
+    );
+    console.log("multipartparams---- ", multipartParams);
+    const uploadId = multipartParams.UploadId;
 
-        res.status(200).json({ uploadId });
-    } catch (err) {
-        console.error('Error initializing upload:', err);
-        next(err);
-    }
-}
-
-// Upload chunk
-export const uploadChunk = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        console.log('Uploading Chunk');
-        const { fileName, chunkIndex, uploadId } = req.body;
-        const bucketName = process.env.AWS_BUCKET_NAME;
-        const s3Client = getAwsClient();
-
-        const partParams = {
-            Bucket: bucketName,
-            Key: fileName,
-            UploadId: uploadId,
-            PartNumber: parseInt(chunkIndex) + 1,
-            Body: req.file?.buffer,
-        };
-
-        const data = await s3Client.send(
-            new UploadPartCommand(partParams)
-        )
-        console.log("data------- ", data);
-        res.status(200).json({ success: true });
-    } catch (err) {
-        console.error('Error uploading chunk:', err);
-        next(err);
-    }
+    res.status(200).json({ uploadId });
+  } catch (err) {
+    console.error("Error initializing upload:", err);
+    next(err);
+  }
 };
 
-export const completeUpload = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        console.log('Completing Upload');
-        const { fileName, totalChunks, uploadId, title, description, author } = req.body;
-        console.log(title, description, author);
+// Upload chunk
+export const uploadChunk = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    console.log("Uploading Chunk");
+    const { fileName, chunkIndex, uploadId } = req.body;
+    const bucketName = process.env.AWS_BUCKET_NAME;
+    const s3Client = getAwsClient();
 
-        const uploadedParts = [];
+    const partParams = {
+      Bucket: bucketName,
+      Key: fileName,
+      UploadId: uploadId,
+      PartNumber: parseInt(chunkIndex) + 1,
+      Body: req.file?.buffer,
+    };
 
-        // Build uploadedParts array from request body
-        for (let i = 0; i < totalChunks; i++) {
-            uploadedParts.push({ PartNumber: i + 1, ETag: req.body[`part${i + 1}`] });
-        }
+    const data = await s3Client.send(new UploadPartCommand(partParams));
+    console.log("data------- ", data);
+    res.status(200).json({ success: true });
+  } catch (err) {
+    console.error("Error uploading chunk:", err);
+    next(err);
+  }
+};
 
-        const s3Client = getAwsClient();
-        const bucketName = process.env.AWS_BUCKET_NAME;
+export const completeUpload = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    console.log("Completing Upload");
+    const { fileName, totalChunks, uploadId, title, description, author } =
+      req.body;
+    console.log(title, description, author);
 
-        const completeParams = {
-            Bucket: bucketName,
-            Key: fileName,
-            UploadId: uploadId,
-        };
+    const uploadedParts = [];
 
-        // Listing parts using promise
-        const data = await s3Client.send(
-            new ListPartsCommand(completeParams)
-        );
-
-        if (!data.Parts) {
-            throw new InternalServerError("Internal Server Error");
-        }
-
-        const parts = data.Parts.map(part => ({
-            ETag: part.ETag,
-            PartNumber: part.PartNumber
-        }));
-
-        const completeRequest = {
-            ...completeParams,
-            MultipartUpload: {
-                Parts: parts
-            }
-        }
-
-        // Completing multipart upload using promise
-        const uploadResult = await s3Client.send(
-            new CompleteMultipartUploadCommand(completeRequest)
-        );
-
-        console.log("data----- ", uploadResult);
-
-        return res.status(200).json({ message: "Uploaded successfully!!!" });
-    } catch (err) {
-        console.error(err);
-        next(err);
+    // Build uploadedParts array from request body
+    for (let i = 0; i < totalChunks; i++) {
+      uploadedParts.push({ PartNumber: i + 1, ETag: req.body[`part${i + 1}`] });
     }
-}
+
+    const s3Client = getAwsClient();
+    const bucketName = process.env.AWS_BUCKET_NAME;
+
+    const completeParams = {
+      Bucket: bucketName,
+      Key: fileName,
+      UploadId: uploadId,
+    };
+
+    // Listing parts using promise
+    const data = await s3Client.send(new ListPartsCommand(completeParams));
+
+    if (!data.Parts) {
+      throw new InternalServerError("Internal Server Error");
+    }
+
+    const parts = data.Parts.map((part) => ({
+      ETag: part.ETag,
+      PartNumber: part.PartNumber,
+    }));
+
+    const completeRequest = {
+      ...completeParams,
+      MultipartUpload: {
+        Parts: parts,
+      },
+    };
+
+    // Completing multipart upload using promise
+    const uploadResult = await s3Client.send(
+      new CompleteMultipartUploadCommand(completeRequest)
+    );
+
+    console.log("data----- ", uploadResult);
+
+    return res.status(200).json({ message: "Uploaded successfully!!!" });
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
+};
--- upload-service/src/index.ts
+++ upload-service/src/index.ts
@@ -1,45 +1,41 @@
-import express, { NextFunction, Request, Response } from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import uploadRouter from './routes/upload.route';
-import errorHandler from './middlewares/errorHandler.middleware';
-import { NotFoundError } from './errors/app.error';
-import { getAwsClient } from './utils/awsClient';
-import { ListBucketsCommand, ListObjectsCommand } from '@aws-sdk/client-s3';
+import express, { NextFunction, Request, Response } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import uploadRouter from "./routes/upload.route";
+import errorHandler from "./middlewares/errorHandler.middleware";
+import { NotFoundError } from "./errors/app.error";
+import { getAwsClient } from "./utils/awsClient";
+import { ListBucketsCommand, ListObjectsCommand } from "@aws-sdk/client-s3";
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cors({ allowedHeaders: ["*"], origin: 'localhost:5173' }));
-
+app.use(cors());
 
 app.use(express.json());
-app.use('/upload', uploadRouter);
-app.get('/status', (req, res) => {
-  res.send({ status: 'in service' });
+app.use("/upload", uploadRouter);
+app.get("/status", (req, res) => {
+  res.send({ status: "in service" });
 });
 
-
-app.get('/s3', async (req: Request, res: Response, next: NextFunction) => {
+app.get("/s3", async (req: Request, res: Response, next: NextFunction) => {
   try {
     const buckets = await getAwsClient().send(
       new ListObjectsCommand({ Bucket: "raw-videos-bucket" })
     );
     res.json(buckets);
   } catch (error) {
-    next(error)
+    next(error);
   }
 });
 
-
 //catch routes KEEP IT AT THE END
 app.use((req: Request, res: Response, next: NextFunction) => {
-  next(new NotFoundError())
-})
+  next(new NotFoundError());
+});
 app.use(errorHandler);
-
 
 app.listen(3000, () => {
   console.log("listening on port 3000");
-})
+});
