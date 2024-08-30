65ee892c5c65c116c858fb9b22fe1b855179a9c2
drshnn <darshansu156@gmail.com>
2024-08-30 23:11:10 +0530

    fix: corrected the import

--- api-gateway/src/controllers/private.controller.ts
+++ api-gateway/src/controllers/private.controller.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from "express"
 import { IRequest } from "../types/jwt.type"
 
-export const getPrivateData = (req: IRequest, res: Response, next: NextFunction) => {
+export const getPrivateData = (req: Request, res: Response, next: NextFunction) => {
     res.status(200).json({ success: true, data: "you got access to the private data in this route" })
 }
